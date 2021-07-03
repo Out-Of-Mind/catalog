6f3429137ab6ac04287e8a0b7f213d30004995ab
Out-Of-Mind <kt.2488482@gmail.com>
2021-07-03 12:44:20 +0300

    gofmt for middlewares.go

--- backend/app/middlewares/middlewares.go
+++ backend/app/middlewares/middlewares.go
@@ -1,62 +1,62 @@
 package middlewares
 
 import (
-    vars "github.com/out-of-mind/catalog/variables"
-    "github.com/go-redis/redis/v8"
-    "github.com/gorilla/csrf"
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/csrf"
+	vars "github.com/out-of-mind/catalog/variables"
 
-    "net/http"
+	"net/http"
 )
 
 func LoggingMiddleware(next http.Handler) http.Handler {
-    vars.Log.Debug("Logging middleware")
+	vars.Log.Debug("Logging middleware")
 
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-        vars.Log.Println(r.RequestURI)
+		vars.Log.Println(r.RequestURI)
 
-        if r.RequestURI == "/login" || r.RequestURI == "/register" || r.RequestURI == "/api" { // test only (!=) in prod set to (==)
-            vars.Log.Debug("Next handler without cookies set")
-            next.ServeHTTP(w, r)
-        } else {
-            c, err := r.Cookie("session_token")
-            if err != nil {
-                if err == http.ErrNoCookie {
-                    vars.Log.Debug("no cookie, redirect")
-                    http.Redirect(w, r, "login", http.StatusTemporaryRedirect)
-                    return
-                }
-                vars.Log.Debug("bad request")
-                w.WriteHeader(http.StatusBadRequest)
-                return
-            }
-            sessionToken := c.Value
+		if r.RequestURI == "/login" || r.RequestURI == "/register" || r.RequestURI == "/api" { // test only (!=) in prod set to (==)
+			vars.Log.Debug("Next handler without cookies set")
+			next.ServeHTTP(w, r)
+		} else {
+			c, err := r.Cookie("session_token")
+			if err != nil {
+				if err == http.ErrNoCookie {
+					vars.Log.Debug("no cookie, redirect")
+					http.Redirect(w, r, "login", http.StatusTemporaryRedirect)
+					return
+				}
+				vars.Log.Debug("bad request")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			sessionToken := c.Value
 
-            _, err = vars.Cache.Get(vars.CTX, sessionToken).Result()
-            if err == redis.Nil {
-                vars.Log.Debug("no cookie in redis, redirect")
-                http.Redirect(w, r, "login", http.StatusTemporaryRedirect)
-                return
-            } else if err != nil {
-                vars.Log.Debug(err)
-            } else {
-                vars.Log.Debug("cookie found, next")
-                next.ServeHTTP(w, r)
-            }
-        }
-    })
+			_, err = vars.Cache.Get(vars.CTX, sessionToken).Result()
+			if err == redis.Nil {
+				vars.Log.Debug("no cookie in redis, redirect")
+				http.Redirect(w, r, "login", http.StatusTemporaryRedirect)
+				return
+			} else if err != nil {
+				vars.Log.Debug(err)
+			} else {
+				vars.Log.Debug("cookie found, next")
+				next.ServeHTTP(w, r)
+			}
+		}
+	})
 }
 
 func CSRFMiddleware(next http.Handler) http.Handler {
-    vars.Log.Debug("CSRF middleware")
+	vars.Log.Debug("CSRF middleware")
 
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.RequestURI == "/login" || r.RequestURI == "/register" {
-            CSRF := csrf.Protect(vars.Secret, csrf.Secure(false))
-            // csrf.Secure(false) only for debug
-            CSRF(next).ServeHTTP(w, r)
-        } else {
-            next.ServeHTTP(w, r)
-        }
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.RequestURI == "/login" || r.RequestURI == "/register" {
+			CSRF := csrf.Protect(vars.Secret, csrf.Secure(false))
+			// csrf.Secure(false) only for debug
+			CSRF(next).ServeHTTP(w, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
 }
