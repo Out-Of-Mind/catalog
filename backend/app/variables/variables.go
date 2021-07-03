bbc361e5d26d756bf437e5f59bca4cce40963e39
Out-Of-Mind <kt.2488482@gmail.com>
2021-07-03 12:40:47 +0300

    gofmt for variables.go

--- backend/app/variables/variables.go
+++ backend/app/variables/variables.go
@@ -3,16 +3,16 @@ package variables
 import (
 	"github.com/go-redis/redis/v8"
 	"github.com/sirupsen/logrus"
-	
-	"database/sql"
+
 	"context"
+	"database/sql"
 )
 
 var (
-	DB *sql.DB
-	Cache *redis.Client
-	CTX = context.Background()
-	Log *logrus.Logger
-	Secret []byte
+	DB          *sql.DB
+	Cache       *redis.Client
+	CTX         = context.Background()
+	Log         *logrus.Logger
+	Secret      []byte
 	TemplateDir string
 )
