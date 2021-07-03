603797415fdc646785fbc3ed5879e1a412e2b724
Out-Of-Mind <kt.2488482@gmail.com>
2021-07-03 12:42:20 +0300

    gofmt for structures.go

--- backend/app/structures/structures.go
+++ backend/app/structures/structures.go
@@ -6,45 +6,45 @@ import (
 
 type RequestDataJSON struct {
 	CategoryName string `json:"category_name"`
-	ItemName string `json:"item_name"`
-	JWT string `json:"jwt"`
-	RJWT string `json:"rjwt"`
+	ItemName     string `json:"item_name"`
+	JWT          string `json:"jwt"`
+	RJWT         string `json:"rjwt"`
 }
 
 type RequestJSON struct {
-	Action string `json:"action"`
-	JWT string `json:"jwt_token"`
-	Data RequestDataJSON `json:"data"`
+	Action string          `json:"action"`
+	JWT    string          `json:"jwt_token"`
+	Data   RequestDataJSON `json:"data"`
 }
 
 type ResponseDataJSON struct {
-	JWT string `json:"jwt"`
+	JWT  string `json:"jwt"`
 	RJWT string `json:"rjwt"`
 
 	Error string `json:"error"`
 }
 
 type ResponseJSON struct {
-	Succes bool `json:"success"`
-	Data ResponseDataJSON`json:"data"`
+	Succes bool             `json:"success"`
+	Data   ResponseDataJSON `json:"data"`
 }
 
 type IndexItems struct {
 	JWT, RJWT string
-	Items []IndexData
+	Items     []IndexData
 }
 
 type IndexData struct {
-	ID int64
+	ID           int64
 	CategoryName string
-	CategoryID string
-	ItemNames []string
+	CategoryID   string
+	ItemNames    []string
 }
 
 type ByID []IndexData
 
-func (a ByID) Len() int { return len(a) }
-func (a ByID) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByID) Len() int           { return len(a) }
+func (a ByID) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByID) Less(i, j int) bool { return a[i].ID < a[j].ID }
 
 type ErrorTemplate struct {
@@ -54,25 +54,25 @@ type ErrorTemplate struct {
 
 type LoginData struct {
 	CSRFToken string
-	Error ErrorTemplate
+	Error     ErrorTemplate
 }
 
 type RegisterData LoginData
 
 type JWT struct {
-	Header JWTHeader
-	Payload JWTPayload
+	Header    JWTHeader
+	Payload   JWTPayload
 	Signature JWTSignature
 }
 
 type JWTHeader struct {
-	Alg string `json:"alg"`
+	Alg  string `json:"alg"`
 	Type string `json:"type"`
 }
 
 type JWTPayload struct {
-	Exp time.Time `json:"exp"`
-	Value int `json:"value"`
+	Exp   time.Time `json:"exp"`
+	Value int       `json:"value"`
 }
 
 type JWTSignature struct {
